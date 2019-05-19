1d65bcf1bff2b24a93767259cb2f3e389e6cbcb8
somilasthana <somilasthana@gmail.com>
2019-05-19 22:09:21 +1000

    Create faceswap_chervonij.py

---
+++ faceswap/faceswap_chervonij.py
@@ -0,0 +1,21 @@
+# This is taken from https://colab.research.google.com/github/chervonij/DFL-Colab/blob/master/DFL_Colab_Demo.ipynb#scrollTo=JG-f2WqT4fLK
+
+
+
+/*
+  !git clone https://github.com/iperov/DeepFaceLab.git
+
+
+!pip install -r /content/DeepFaceLab/requirements-colab.txt
+!pip install --upgrade scikit-image
+
+!wget -q --no-check-certificate -r 'https://docs.google.com/uc?export=download&id=1hTH2h6l_4kKrczA8EkN6GyuXx4lzmCnK' -O pretrain_CelebA.zip
+!mkdir /content/pretrain
+!unzip -q /content/pretrain_CelebA.zip -d /content/pretrain/
+!rm /content/pretrain_CelebA.zip
+*/
+
+if not Path("/content/workspace").exists():
+
+
+print("Done!")
